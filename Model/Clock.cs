3471812c06d95e6ebecc7c40694fa9f33094c8a9
KrzysztofB <ka.badkowski@gmail.com>
2019-05-27 00:06:52 +0200

    Add project files.

---
+++ Model/Clock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVVM_Clock.Model
+{
+    public class Clock : INotifyPropertyChanged
+    {
+        private int _hour = DateTime.Now.Hour;
+
+        public int Hour
+        {
+            get { return _hour; }
+            set { _hour = value; OnPropertyChanged("Second"); }
+        }
+
+        private int _minute = DateTime.Now.Minute;
+
+        public int Minute
+        {
+            get { return _minute; }
+            set { _minute = value; OnPropertyChanged("Second"); }
+        }
+
+        private int _second = DateTime.Now.Second;
+
+        public int Second
+        {
+            get { return _second; }
+            set { _second = value; OnPropertyChanged("Second"); }
+        }
+
+        private Timer _timer;
+
+        public Timer Timer
+        {
+            get { return _timer; }
+            set { _timer = value; }
+        }
+
+        public static Clock operator ++(Clock c)
+        {
+            if (c.Second++ == 59)
+            {
+                c.Second = 0;
+                if (c.Minute++ == 59)
+                {
+                    c.Minute = 0;
+                    c.Hour++;
+                    if (c.Hour == 24)
+                    {
+                        c.Hour = 0;
+                    }
+                }
+            }
+            return c;
+        }
+
+        public Clock()
+        {
+            Timer = new Timer
+            {
+                Interval = 1000,
+                Enabled = true
+            };
+            Timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler ph = PropertyChanged;
+            if (ph != null)
+            {
+                ph(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ViewModel/ClockViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MVVM_Clock.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVVM_Clock.ViewModel
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        public string Title { get; set; } = "Clock App";
+
+        private Clock _clock;
+
+        public Clock Clock
+        {
+            get { return _clock; }
+            set { _clock = value; NotifyOnPropertyChanged("Clock"); }
+        }
+
+        public ClockViewModel()
+        {
+            Clock = new Clock();
+            Clock.Timer.Tick += new EventHandler(this.Tick);
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            Clock++;
+        }
+
+        private void NotifyOnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
